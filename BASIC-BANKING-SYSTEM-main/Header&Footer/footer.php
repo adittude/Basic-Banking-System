09db24e9f023a2940ffb30fd948322e731268d5d
adittude <56556234+adittude@users.noreply.github.com>
2021-09-18 16:44:12 +0530

    Add files via upload

---
+++ BASIC-BANKING-SYSTEM-main/Header&Footer/footer.php
@@ -0,0 +1,41 @@
+<?php
+include 'sql/common.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--Latest compiled and minified JavaScript--> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/style.css">
+<title>Aditya's Virtual Banking System</title>
+</head>
+<body>
+<footer>
+<hr>
+    <div class="row text-center">
+      <div class="col-md-6">
+        <p><li><a target="_blank" class="The Sparks Foundation" href="https://www.linkedin.com/company/the-sparks-foundation/mycompany/"> The Sparks Foundation</a> </p>
+
+</div>
+      <div class="col-md-6 text-center">
+        <ul class="social-icons">
+          Connect :
+          <li><a target="_blank" class="LinkedIn" href="https://www.linkedin.com/in/aditya-kini-457aa6218/"> Aditya Kini <i
+                class="fab fa-linkedin fa-2x"></i></a>
+              <a target="_blank" class="Instagram" href="https://www.instagram.com/adittude_25/"> <i 
+                class="fab fa-instagram fa-2x"></i></a>
+                <a target="_blank" class="GitHub" href="https://github.com/adittude"> <i 
+                  class="fab fa-github fa-2x"></i></a>
+            </li>
+
+        </ul>
+      </div>
+    </div>
+</footer>
+</body>
+</html>
